f5729898f0c321e08f01ba46c19aece8fe17ccc9
Scott Nicholes <scott93gps@gmail.com>
2019-01-26 20:57:59 -0700

    Deleted old code.  Added headers for item cards

--- web/shopping-cart/browse.php
+++ web/shopping-cart/browse.php
@@ -9,24 +9,15 @@
         include 'header.php';
         ?>
         <div class="main-content">
-            <h2>About Us</h2>
+            <h2>Browse</h2>
             <div class="card">
-                We are Edna Mode's Costume Shop!  You need no look further, darling.  Whether it's a supersuit or a supermodel suit, we have everything you need to make you look good and get the job done.
-                <br>
-                <br>
-                We have 3 types of products for you, darling:
-                <h3>Mr. Incredible Supersuit</h3>
-                <img src="https://c1.staticflickr.com/6/5258/5403621899_4cd745ebd5_b.jpg" />
-                <br>
-                This supersuit can fit any heavyweight ready to get the job done!
-                <h3>Frozone Supersuit</h3>
-                <img src="https://d13ezvd6yrslxm.cloudfront.net/wp/wp-content/images/incredibles-2-frozone-e1539973596318-700x371.jpeg" />
-                <br>
-                Make sure you always know where your supersuit is!
-                <h3>Elastigirl!</h3>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr9_IYsssufrqkn25i6KFdtacpk-wcGz0BZfg1DInYdGN3DlBL" />
-                <br>
-                This is my favorite!  It can stretch to any size you want!
+                <h3>Cool Surfboard</h3>
+            </div>
+            <div class="card">
+                <h3>Awesome Surfboard</h3>
+            </div>
+            <div class="card">
+                <h3>Gnarly Surboard Dude!</h3>
             </div>
         </div>
     </body>
