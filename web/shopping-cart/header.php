05f3177af45642ebf42a93189a46310afb85a7c1
Scott Nicholes <scott93gps@gmail.com>
2019-01-26 22:11:39 -0700

    Removed flexbox styles.  Rerouting to working on adding to cart functionality

--- web/shopping-cart/header.php
+++ web/shopping-cart/header.php
@@ -3,17 +3,6 @@ $activePage = basename($_SERVER['PHP_SELF'], ".php");
 ?>
 <div class="header">
     <div class="card">
-        <div class="row">
-            <div class="column">
-                <img src="https://upload.wikimedia.org/wikipedia/en/thumb/2/29/Count_von_Count_kneeling.png/200px-Count_von_Count_kneeling.png" />
-            </div>
-            <div class="column">
-                <h1>The Count's Organ Mercantile</h1>
-            </div>
+        <h1>Scott's Organ Mercantile</h1>
     </div>
-    <!-- <ul class="navBar">
-        <li class="<?php echo ($activePage == 'home') ? 'activeNav':''; ?>"><a href="/home.php">Home</a></li>
-        <li class="<?php echo ($activePage == 'about-us') ? 'activeNav':''; ?>"><a href="/about-us.php">About Us</a></li>
-        <li class="<?php echo ($activePage == 'login') ? 'activeNav':''; ?>"><a href="/login.php">Login</a></li>
-    </ul> -->
 </div>
