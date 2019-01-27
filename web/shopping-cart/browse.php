e8a3dd23b1f3dc3fd7c01d8be8de957ae46b224f
Scott Nicholes <scott93gps@gmail.com>
2019-01-26 22:38:04 -0700

    Set up form instead of button

--- web/shopping-cart/browse.php
+++ web/shopping-cart/browse.php
@@ -13,12 +13,14 @@
             <div class="card">
                 <img src="https://pipeorgandatabase.org/photos/CA/SanFrancisco.DaviesSympho.FratelliRu.1020.222726.jpg" />
                 <h3>Ruffati - Davies Concert Hall</h3>
-                <select id="quantity">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                </select>
-                <button id="ruffati-add-cart">Add To Cart</button>
+                <form action="add.php" method="post">
+                    <select id="quantity">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                    </select>
+                    <input type="submit" id="ruffati-add-cart" />
+                </form>
             </div>
             <div class="card">
                 <h3>Awesome Surfboard</h3>
