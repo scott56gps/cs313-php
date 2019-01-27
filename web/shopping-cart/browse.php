635fe4df132f80d8deb4e88a91f5917e6efe1a9d
Scott Nicholes <scott93gps@gmail.com>
2019-01-26 20:49:07 -0700

    Initial Commit.  Copied files over from Teach02

---
+++ web/shopping-cart/browse.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>About Us - Edna's Costume Shop</title>
+        <link href="styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <?php
+        include 'header.php';
+        ?>
+        <div class="main-content">
+            <h2>About Us</h2>
+            <div class="card">
+                We are Edna Mode's Costume Shop!  You need no look further, darling.  Whether it's a supersuit or a supermodel suit, we have everything you need to make you look good and get the job done.
+                <br>
+                <br>
+                We have 3 types of products for you, darling:
+                <h3>Mr. Incredible Supersuit</h3>
+                <img src="https://c1.staticflickr.com/6/5258/5403621899_4cd745ebd5_b.jpg" />
+                <br>
+                This supersuit can fit any heavyweight ready to get the job done!
+                <h3>Frozone Supersuit</h3>
+                <img src="https://d13ezvd6yrslxm.cloudfront.net/wp/wp-content/images/incredibles-2-frozone-e1539973596318-700x371.jpeg" />
+                <br>
+                Make sure you always know where your supersuit is!
+                <h3>Elastigirl!</h3>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr9_IYsssufrqkn25i6KFdtacpk-wcGz0BZfg1DInYdGN3DlBL" />
+                <br>
+                This is my favorite!  It can stretch to any size you want!
+            </div>
+        </div>
+    </body>
+</html>
---
+++ web/shopping-cart/header.php
@@ -0,0 +1,13 @@
+<?php
+$activePage = basename($_SERVER['PHP_SELF'], ".php");
+?>
+<div class="header">
+    <div class="card">
+        <h1>Edna's Costume Shop</h1>
+    </div>
+    <ul class="navBar">
+        <li class="<?php echo ($activePage == 'home') ? 'activeNav':''; ?>"><a href="/home.php">Home</a></li>
+        <li class="<?php echo ($activePage == 'about-us') ? 'activeNav':''; ?>"><a href="/about-us.php">About Us</a></li>
+        <li class="<?php echo ($activePage == 'login') ? 'activeNav':''; ?>"><a href="/login.php">Login</a></li>
+    </ul>
+</div>
