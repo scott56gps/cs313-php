85111be83c1d7357a247fc8f09f5e0fe642cda0b
Scott Nicholes <scott93gps@gmail.com>
2019-01-26 21:00:25 -0700

    Testing out header

--- web/shopping-cart/header.php
+++ web/shopping-cart/header.php
@@ -3,11 +3,11 @@ $activePage = basename($_SERVER['PHP_SELF'], ".php");
 ?>
 <div class="header">
     <div class="card">
-        <h1>Edna's Costume Shop</h1>
+        <h1>The Count's Organ Mercantile</h1>
     </div>
-    <ul class="navBar">
+    <!-- <ul class="navBar">
         <li class="<?php echo ($activePage == 'home') ? 'activeNav':''; ?>"><a href="/home.php">Home</a></li>
         <li class="<?php echo ($activePage == 'about-us') ? 'activeNav':''; ?>"><a href="/about-us.php">About Us</a></li>
         <li class="<?php echo ($activePage == 'login') ? 'activeNav':''; ?>"><a href="/login.php">Login</a></li>
-    </ul>
+    </ul> -->
 </div>
