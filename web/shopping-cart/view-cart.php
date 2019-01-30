6cfc55280ad330be9d697b0022c4cf25959a517f
Scott Nicholes <scott93gps@gmail.com>
2019-01-30 00:25:16 -0700

    Deleted body.  Starting with clean slate

--- web/shopping-cart/view-cart.php
+++ web/shopping-cart/view-cart.php
@@ -18,60 +18,7 @@ function generateOptions() {
         include 'header.php';
         ?>
         <div class="main-content">
-            <div class="card">
-                <h3>Ruffatti - Davies Concert Hall</h3>
-                <div class="container">
-                    <img id="ruffatti-image" src="https://pipeorgandatabase.org/photos/CA/SanFrancisco.DaviesSympho.FratelliRu.1020.222726.jpg" />
-                    <div class="item">
-                        <span><strong>Quantity</strong></span>
-                        <select id="ruffatti-quantity">
-                            <?php
-                            generateOptions();
-                            ?>
-                        </select>
-                        <br>
-                        <button id="ruffatti" onclick="parseItem(this.id);">Add To Cart</button>
-                        <br>
-                        <span id="ruffatti-feedback"></span>
-                    </div>
-                </div>
-            </div>
-            <div class="card">
-            <h3>Rieger - Christ Church, Oxford, England</h3>
-                <div class="container">
-                    <img id="rieger-image" src="https://pipedreams.publicradio.org/gallery/united_kingdom/images/oxford_christ-church_rieger_lg.jpg" />
-                    <div class="item">
-                        <span><strong>Quantity</strong></span>
-                        <select id="rieger-quantity">
-                            <?php
-                            generateOptions();
-                            ?>
-                        </select>
-                        <br>
-                        <button id="rieger" onclick="parseItem(this.id);">Add To Cart</button>
-                        <br>
-                        <span id="rieger-feedback"></span>
-                    </div>
-                </div>
-            </div>
-            <div class="card">
-            <h3>Glatter-Gotz - Claremont United Church of Christ Congregational</h3>
-                <div class="container">
-                    <img id="glatter-image" src="http://pipedreams.publicradio.org/gallery/us_west/california/images/claremont_ucc_glatter-gotz-rosales_lg.jpg" />
-                    <div class="item">
-                        <span><strong>Quantity</strong></span>
-                        <select id="glatter-quantity">
-                        <?php
-                        generateOptions();
-                        ?>
-                        </select>
-                        <br>
-                        <button id="glatter" onclick="parseItem(this.id);">Add To Cart</button>
-                        <br>
-                        <span id="glatter-feedback"></span>
-                    </div>
-                </div>
-            </div>
+            
         </div>
     </body>
     <script src="browse.js"></script>
