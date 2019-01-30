76a334a8797491c6d627235ac9641d550dadb8fc
Scott Nicholes <scott93gps@gmail.com>
2019-01-29 21:40:47 -0700

    Changed HTML and CSS structure

--- web/shopping-cart/browse.php
+++ web/shopping-cart/browse.php
@@ -21,19 +21,23 @@ foreach ($_SESSION as $key => $value) {
         include 'header.php';
         ?>
         <div class="main-content">
-            <?php
-            include 'nav-card.php';
-            ?>
             <div class="card">
-                <h3>Ruffati - Davies Concert Hall</h3>
-                <img id="rufatti-image" src="https://pipeorgandatabase.org/photos/CA/SanFrancisco.DaviesSympho.FratelliRu.1020.222726.jpg" />
-                <span id="rufatti-feedback"></span>
-                <select id="rufatti-quantity">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                </select>
-                <button id="rufatti" onclick="parseItem(this.id);">Add To Cart</button>
+                <h3>Ruffatti - Davies Concert Hall</h3>
+                <div class="container">
+                    <img id="ruffatti-image" src="https://pipeorgandatabase.org/photos/CA/SanFrancisco.DaviesSympho.FratelliRu.1020.222726.jpg" />
+                    <div class="item">
+                        <span><strong>Quantity</strong></span>
+                        <select id="ruffatti-quantity">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                        </select>
+                        <br>
+                        <button id="ruffatti" onclick="parseItem(this.id);">Add To Cart</button>
+                        <br>
+                        <span id="ruffatti-feedback"></span>
+                    </div>
+                </div>
             </div>
             <div class="card">
                 <h3>Awesome Surfboard</h3>
--- web/shopping-cart/header.php
+++ web/shopping-cart/header.php
@@ -1,5 +1,24 @@
-<div class="header">
-    <div class="card">
-        <h1>Scott's Organ Mercantile</h1>
+<?php
+$activePage = basename($_SERVER['PHP_SELF'], ".php");
+?>
+<div id="header-card" class="card">
+    <h1>Scott's Organ Mercantile</h1>
+    <div id="nav-card" class="card">
+    <?php
+    switch ($activePage) {
+        case 'browse':
+            echo '<h2>Browse</h2>';
+            break;
+        case 'view-cart':
+            echo '<h2>View Cart</h2>';
+            break;
+        case 'checkout':
+            echo '<h2>Checkout</h2>';
+            break;
+        case 'confirm':
+            echo '<h2>Confirmation</h2>';
+            break;
+    }
+    ?>
     </div>
 </div>
