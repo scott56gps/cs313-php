7329de78bb2e32a1b72a66dfc525045d100a72e4
Scott Nicholes <scott93gps@gmail.com>
2019-02-05 10:40:15 -0700

    Added movie statement

--- db/05_movies.sql
+++ db/05_movies.sql
@@ -2,3 +2,14 @@ CREATE TABLE actor (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL
 );
+
+-- CREATE TABLE genre (
+--     id SERIAL PRIMARY KEY,
+--     name VARCHAR(50) NOT NULL
+-- );
+
+CREATE TABLE movie (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    year SMALLINT
+);
