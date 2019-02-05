153a0f8b477e5f8f9263754292c4ecc057880ede
Scott Nicholes <scott93gps@gmail.com>
2019-02-04 22:31:16 -0700

    Added prepared statement to example-query.php

--- db/example-query.php
+++ db/example-query.php
@@ -15,13 +15,25 @@ try
 
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  foreach($db->query('SELECT * FROM student') as $row) {
-      echo 'student_id: ' . $row['student_id'];
-      echo 'student_first_name: ' . $row['student_first_name'];
-      echo 'student_last_name: ' . $row['student_last_name'];
-      echo 'teacher_id: ' . $row['teacher_id'];
-      echo 'username: ' . $row['username'];
-      echo 'password: ' . $row['password'];
+  // The manual, not-secure way
+//   foreach($db->query('SELECT * FROM student') as $row) {
+//       echo 'student_id: ' . $row['student_id'];
+//       echo 'student_first_name: ' . $row['student_first_name'];
+//       echo 'student_last_name: ' . $row['student_last_name'];
+//       echo 'teacher_id: ' . $row['teacher_id'];
+//       echo 'username: ' . $row['username'];
+//       echo 'password: ' . $row['password'];
+//   }
+
+  // The secure way
+  $id = 1;
+  $statement = $db->prepare('SELECT student_first_name FROM student WHERE id=:id');
+  $statement->bindValue(':id', $id, PDO::PARAM_INT);
+  $statement->execute();
+  $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+  foreach ($rows as $row) {
+      echo 'studentFirstName: ' . $row['student_first_name'] . '<br/>';
   }
 }
 catch (PDOException $ex)
