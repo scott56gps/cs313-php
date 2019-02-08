601efa2a869822e252289978aee0a629902d70e3
Scott Nicholes <scott93gps@gmail.com>
2019-02-07 21:07:31 -0700

    Changed submit to search

--- web/teach-05/scriptures-form.php
+++ web/teach-05/scriptures-form.php
@@ -1,5 +1,5 @@
 <form action="scriptures.php" method="post">
     <label for="bookInput">Book:</label>
     <input type="text" id="bookInput" name="book" />
-    <input type="submit" value="Submit" />
+    <input type="submit" value="Search" />
 </form>
