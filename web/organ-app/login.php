5d59692b44b3d6aae8d0094f13f5990a0150a460
Scott Nicholes <scott93gps@gmail.com>
2019-02-11 18:37:56 -0700

    Copied over login file from Teach02

--- web/organ-app/login.php
+++ web/organ-app/login.php
@@ -9,5 +9,15 @@
         <?php
         include 'sidebar.php';
         ?>
+        <div class="main-content">
+            <h2>Login</h2>
+            <div class="card">
+                <h3>Log In here Darling!</h3>
+                <form action="/session.php" method="get">
+                    <input type="submit" name="username" value="Administrator" id="adminLoginButton">Log in as Administrator</input>
+                    <input type="submit" name="username" value="Tester" id="testerLogin">Log in as Tester</input>
+                </form>   
+            </div>
+        </div>
     </body>
 </html>
