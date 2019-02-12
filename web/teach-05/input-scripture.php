a396ba1108e05b9260ffadf479bf3cea91096925
Scott Nicholes <scott93gps@gmail.com>
2019-02-12 10:32:28 -0700

    Added 2 new files.  About to test querying

---
+++ web/teach-05/input-scripture.php
@@ -0,0 +1,23 @@
+<?php
+include 'db.php';
+$db = getDb();
+
+// The secure way
+$id = 1;
+$statement = $db->prepare('SELECT id, name FROM topic');
+$statement->execute();
+$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+?>
+<form action="insert-scripture.php" method="post">
+    <label for="bookInput">Book:</label>
+    <input type="text" id="bookInput" name="book" />
+    <input type="text" id="chapterInput" name="chapter" />
+    <input type="text" id="verseInput" name="verse" />
+    <textarea id="contentInput" name="content"></textarea>
+    <?php
+    foreach ($rows as $row) {
+        echo '<input type="checkbox" name="topics[]" value="' . $row['id'] . '" />' . $row['name'];
+    }
+    ?>
+    <input type="submit" value="Insert" />
+</form>
