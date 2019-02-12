d8791b19389e907d646b1bd345ac9979aa3538f2
Scott Nicholes <scott93gps@gmail.com>
2019-02-11 18:45:06 -0700

    Added username and password fields

--- web/organ-app/login.php
+++ web/organ-app/login.php
@@ -12,10 +12,11 @@
         <div class="main-content">
             <h2>Login</h2>
             <div class="card">
-                <h3>Log In here Darling!</h3>
                 <form action="/session.php" method="get">
-                    <input type="submit" name="username" value="Administrator" id="adminLoginButton">Log in as Administrator</input>
-                    <input type="submit" name="username" value="Tester" id="testerLogin">Log in as Tester</input>
+                    <label for="username">Username</label>
+                    <input id="username" type="text" name="username" />
+                    <label for="password">Password</label>
+                    <input id="password" type="text" name="password" />
                 </form>   
             </div>
         </div>
