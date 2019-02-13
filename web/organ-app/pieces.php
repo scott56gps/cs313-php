01f450e5b1b8f223dfe32c7b7de8c5ed736dd422
Scott Nicholes <scott93gps@gmail.com>
2019-02-12 22:20:49 -0700

    Added dynamic content to pieces

--- web/organ-app/pieces.php
+++ web/organ-app/pieces.php
@@ -1,1 +1,26 @@
-<h1>My Pieces</h1>
+<?php
+session_start();
+
+$studentFirstName = $_SESSION['student_first_name'];
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php
+        echo "<title>$studentFirstName's Pieces"
+        ?>
+        <link href="styles.css" rel="stylesheet" />
+    </head>
+
+    <body>
+        <?php
+        include 'sidebar.php';
+        ?>
+        <div class="main-content">
+            <h2>My Pieces</h2>
+            <?php
+            
+            ?>
+        </div>
+    </body>
+</html>
