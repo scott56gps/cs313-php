98c87be2167257e2c3c2d4743d57393d6b7100f6
Scott Nicholes <scott93gps@gmail.com>
2019-02-13 16:16:19 -0700

    Set active page and checked for logged in user

--- web/organ-app/sidebar.php
+++ web/organ-app/sidebar.php
@@ -1,4 +1,19 @@
+<?php
+session_start();
+
+$activePage = basename($_SERVER['PHP_SELF'], ".php");
+
+$isLoggedIn = FALSE;
+
+if (isset($_SESSION['username'])) {
+    $isLoggedIn = TRUE;
+}
+?>
 <div class="sidebar">
-    <a href="#">My Pieces</a>
-    <a href="#">Login</a>
+    <?php
+    if ($isLoggedIn) {
+        echo '<a href="pieces.php">My Pieces</a>';
+    }
+    ?>
+    <a href="login.php">Login</a>
 </div>
