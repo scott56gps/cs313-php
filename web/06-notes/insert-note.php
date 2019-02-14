7fe230ff0e3bbf8a12863e3d56e571a3450a3cfb
Scott Nicholes <scott93gps@gmail.com>
2019-02-14 11:13:54 -0700

    Inserted into database

--- web/06-notes/insert-note.php
+++ web/06-notes/insert-note.php
@@ -9,8 +9,13 @@ $content = htmlspecialchars($_POST['content']);
 
 echo "Hola";
 
-$query = 'SELECT name, course_code FROM course';
+$query = 'INSERT INTO note (date, content, course_id) VALUES (:date, :content, :course_id)';
 $statement = $db->prepare($query);
+$statement->bindValue(':date', $date, PDO::PARAM_STR);
+$statement->bindValue(':content', $content, PDO::PARAM_STR);
+$statement->bindValue(':course_id', $course_id, PDO::PARAM_INT);
 $statement->execute();
-$courses = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+header("Location: notes.php?course_id=$course_id");
 ?>
