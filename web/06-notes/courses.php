fbf8192c5275355fd03703fa1bb11ff3594c0170
Scott Nicholes <scott93gps@gmail.com>
2019-02-14 10:24:28 -0700

    Added courses and db connection file

---
+++ web/06-notes/courses.php
@@ -0,0 +1,24 @@
+<?php
+include 'db.php';
+
+$db = getDb();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Courses</title>
+</head>
+<body>
+    <h1>Notes App</h1>
+
+    <h2>Courses</h2>
+
+    <ul>
+        <li></li>
+        <li></li>
+    </ul>
+</body>
+</html>
