cf00c81c7714395cea29044494222605cfa932b3
Scott Nicholes <scott93gps@gmail.com>
2019-02-15 13:03:31 -0700

    Changed to TRUE

--- web/organ-app/login.php
+++ web/organ-app/login.php
@@ -20,7 +20,7 @@ session_start();
                     <input id="username" type="text" name="username" />
                     <label for="password">Password</label>
                     <input id="password" type="text" name="password" />
-                    <input type="hidden" name="login" value="false" />
+                    <input type="hidden" name="login" value="TRUE" />
                     <input type="submit" value="Log In" />
                 </form>   
             </div>
