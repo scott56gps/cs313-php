64d33be9566dcbfd99f43b5c2597c96918610cbd
Scott Nicholes <scott93gps@gmail.com>
2019-02-25 19:08:55 -0700

    Added clearing of Session Variables array

--- web/organ-app/session.php
+++ web/organ-app/session.php
@@ -98,14 +98,7 @@ function signUp($db) {
 }
 
 function logout() {
-    if (ini_get("session.use_cookies")) {
-        $params = session_get_cookie_params();
-        setcookie(session_name(), '', time() - 42000,
-            $params["path"], $params["domain"],
-            $params["secure"], $params["httponly"]
-        );
-    }
-    
+    $_SESSION = array();
     session_destroy();
 
     header("Location: login.php");
